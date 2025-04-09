13a4d28630766db58c17ccade53f3508f434a10c
John McCrary <john.mccrary@numerator.com>
2025-04-09 09:31:29 -0500

    Update makefile

--- make.sh
+++ make.sh
@@ -1,29 +1,29 @@
 #!/bin/bash
 
-# GNU make is unavailable on my system at the moment, 
+# GNU make is unavailable on my system at the moment,
 # so I have built this simple build script instead.
 
 
-if [ -f hjkl.zip ] 
-then 
+if [ -f hjkl.zip ]
+then
   rm hjkl.zip
 fi
 
-echo 
+echo
 echo "Building the zip file..."
-echo 
+echo
 
-zip -r ./hjkl.zip "./*.js" "./icons/*.png" ./manifest.json ./LICENSE.txt
+zip -r ./hjkl.zip ./*.js ./icons/*.png ./manifest.json ./LICENSE.txt
 
-echo 
+echo
 echo "Listing the zip file..."
-echo 
+echo
 
 unzip -l ./hjkl.zip
 
-echo 
+echo
 echo "Build complete, sha256: $(md5sum hjkl.zip)"
-echo 
+echo
 
 
 
